f81f0ec412539dbe0033de4292905a7c84f7d75d
Asadulloh <sayfullayevasadulla1@gmail.com>
2023-08-29 00:22:42 +0500

    added some code to constant folder and header

--- app/[locale]/components/global/Header.tsx
+++ app/[locale]/components/global/Header.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { ADS_UZ, ADS_RU } from '@/constant'
+import { useTranslations } from 'next-intl'
+import { usePathname } from 'next/navigation'
 const Header = () => {
+  const t = useTranslations()
+  const path = usePathname()
   return (
-    <div>Header</div>
+    <div>
+      {
+        path === "/ru" ? ADS_RU.map((e)=> {
+          return ""
+        }): ADS_UZ.map((e)=> {
+          return ""
+        })
+      }</div>
   )
 }
 
---
+++ constant/index.ts
@@ -0,0 +1,25 @@
+import ICard from "@/interfaces/ICard";
+
+export const ADS_UZ: ICard[] = [
+  {
+    images: ["1.png", "2.png"],
+    title: "totot",
+    oldPrice: 1200,
+    price: 1500,
+    url: "detail/wejfbhwiuefhuiwhfe",
+    manifacturer: "xuy",
+    desc: "ifehwegfbuwygef uywgef wegf uwefgu wyfeguwygfwue fguywfe wuyfeg wytefgweytfgwye tfgwye tfgwye fgweyfwte f",
+  },
+];
+
+export const ADS_RU: ICard[] | Array<ICard> = [
+  {
+    images: ["1.png", "2.png"],
+    title: "попопопоп",
+    oldPrice: 1200,
+    price: 1500,
+    url: "detail/wejfbhwiuefhuiwhfe",
+    manifacturer: "хуй",
+    desc: "цунап цгау цууг гура гцунапгц уапцгну щшвомщшва твоамр8",
+  },
+];

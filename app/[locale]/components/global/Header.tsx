7bd1c035e6366265aff9e90f6264572dbfff6d77
Asadulloh <sayfullayevasadulla1@gmail.com>
2023-08-29 00:23:19 +0500

    fixed

--- app/[locale]/components/global/Header.tsx
+++ app/[locale]/components/global/Header.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { ADS_UZ, ADS_RU } from '@/constant'
 import { useTranslations } from 'next-intl'
 import { usePathname } from 'next/navigation'
+import ICard from '@/interfaces/ICard'
 const Header = () => {
   const t = useTranslations()
   const path = usePathname()
   return (
     <div>
       {
-        path === "/ru" ? ADS_RU.map((e)=> {
+        path === "/ru" ? ADS_RU.map((e: ICard)=> {
           return ""
-        }): ADS_UZ.map((e)=> {
+        }): ADS_UZ.map((e: ICard)=> {
           return ""
         })
       }</div>

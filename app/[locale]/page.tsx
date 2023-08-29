ead608cea3d64b3d207eac4b6132b41b90b221c4
Asliddin <132223128+hackerlop@users.noreply.github.com>
2023-08-29 18:32:45 +0500

    sadsa

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,20 +1,21 @@
-"use client"
-import Image from 'next/image'
-import styles from '@/styles/page.module.css'
-import { useTranslations } from 'next-intl'
-import Container from './components/global/Container'
-import Header from './components/global/Header'
-import TopHeader from './components/global/TopHeader'
-import Categories from './components/global/Categories'
+"use client";
+import Image from "next/image";
+import styles from "@/styles/page.module.css";
+import { useTranslations } from "next-intl";
+import Container from "./components/global/Container";
+import Header from "./components/global/Header";
+import TopHeader from "./components/global/TopHeader";
+import Categories from "./components/global/Categories";
+import Footer from "./components/global/Footer";
 
 export default function Home() {
-  const t = useTranslations()
+  const t = useTranslations();
   return (
     <main className={styles.main}>
-      <TopHeader/>
-      <Header/>
-      <Categories/>
-      {/* <h1>{t("title")}</h1>
+      <TopHeader />
+      <Header />
+      <Categories />
+      <h1 style={{ marginTop: 140 }}>{t("title")}</h1>
       <Container>
         <div>
           <h1>title</h1>
@@ -29,7 +30,9 @@ export default function Home() {
           <h1>title</h1>
           <p>description</p>
         </div>
-      </Container> */}
+      </Container>
+        <Footer />
+
     </main>
-  )
+  );
 }

427d7c22eec2ae79466ac94b19c7e42195c0dd35
AvazbekShukurjonov <94116763+AvazbekShukurjonov@users.noreply.github.com>
2023-08-29 22:22:11 +0500

    Added some codes from Ads.tsx.

--- constant/index.ts
+++ constant/index.ts
@@ -2,24 +2,24 @@ import ICard from "@/interfaces/ICard";
 
 export const ADS_UZ: ICard[] = [
   {
-    images: ["1.png", "2.png"],
+    images: ["/images/blackpods.png", "/images/phone.png",  "/images/redpods.png"],
     title: "totot",
     oldPrice: 1200,
     price: 1500,
     url: "detail/wejfbhwiuefhuiwhfe",
-    manifacturer: "xuy",
-    desc: "ifehwegfbuwygef uywgef wegf uwefgu wyfeguwygfwue fguywfe wuyfeg wytefgweytfgwye tfgwye tfgwye fgweyfwte f",
+    manifacturer: "JBL Quantum ONE",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing eilt. Tempore libero, delectus aut sint dolore facere voluptatum quidem alias asperiores veniam non explicabo, sapiente dolor numquam corporis vero iste consectetur.",
   },
 ];
 
 export const ADS_RU: ICard[] | Array<ICard> = [
   {
-    images: ["1.png", "2.png"],
-    title: "попопопоп",
+    images: ["/images/blackpods.png", "/images/phone.png",  "/images/redpods.png"],
+    title: "Лорем, ipsum.",
     oldPrice: 1200,
     price: 1500,
     url: "detail/wejfbhwiuefhuiwhfe",
-    manifacturer: "хуй",
-    desc: "цунап цгау цууг гура гцунапгц уапцгну щшвомщшва твоамр8",
+    manifacturer: "JBL Квантум ONE  ",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing eilt. Tempore libero, delectus aut sint dolore facere voluptatum quidem alias asperiores veniam non explicabo, sapiente dolor numquam corporis vero iste consectetur.",
   },
 ];
